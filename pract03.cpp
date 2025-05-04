c4a7e378dce9f806b28fb5e9753b9872ed4b0dbd
GauriKharad <gaurikharad@gmail.com>
2025-05-04 23:22:09 +0530

    pract03.cpp

--- pract03.cpp
+++ pract03.cpp
@@ -2,325 +2,238 @@
 #include<queue>
 using namespace std;
 
-struct Node {
+struct node {
     int data;
-    Node *left;
-    Node *right;
-    Node(int x) : data(x),left(NULL),right (NULL){};
+    node *left, *right;
+    node(int val) : data(val), left(nullptr), right(nullptr) {} 
 };
 
+class BST {
+public:
+    node* root = nullptr;
 
-class Traversal{
-    public:
-Node *createNode(int value){
-    Node *temp = new Node(value);
-    return temp;
-}
-
-
-int longestPath(Node *root){
-    if(root == NULL){
-        return 0;
-    }
-    else{
-        int leftpath = longestPath(root -> left);
-        int rightpath = longestPath(root -> right);
-        return max(leftpath,rightpath) + 1;
-    }
-}
-
-int minValue(Node *root){
-    if(root == NULL){
-        cout<<"Tree Is Empty\n";
-        return 0;
-    }
-    else{
-        if(root -> left){
-            root = root -> left;
-        }
-        return root -> data;
-    }
-    
-}
-
-Node *swapTree(Node *root){
-   
-    if(root){
-        swap(root -> left, root -> right);
-        swapTree(root -> left);
-        swapTree(root -> right);
-    }
-    return root;
-}
-
-Node *insertNode(Node *root, int data){
-    Node * temp = new Node(data);
-    if(root == NULL){
-        root = temp;
-    }
-    else{
-        if(root -> data < data){
-            root -> right = insertNode(root -> right,data);
-        }
-        else{
-            root -> left = insertNode(root -> left, data);
-        }
-    }
-    return root;
-}
-
-Node *findMin(Node *root){
-    while(root -> left != NULL){
-        root = root -> left;
-    }
-    return root;
-}
-
-Node *Delete(Node *root,int key){
-    
-    if(root == NULL){
-        cout<<"Empty.\n";
-    }
-    if(key > root -> data){
-        return Delete(root -> right , key);
-    }
-    else if(key < root -> data){
-        return Delete(root -> left , key);
-    }
-    else{
-
-        // no child
-        if(root -> left == NULL && root -> right == NULL){
-            delete root;
-        }
-        //single child
-        else if(root -> left == NULL){
-            Node *temp = root -> right;//right child is present.
-            delete root;
-            return temp;
-        }
-        else if(root -> right == NULL){
-            Node * temp = root -> left;//left child is present.
-            delete root;
-            return temp;
+    void insert(int val) { 
+        node* newnode = new node(val); 
+        if (root == nullptr) {
+            root = newnode;
+            cout << "Root Node Inserted.\n";
+            return;
         }
 
-        else{
-            Node * successor = findMin(root -> right);
-            root -> data = successor -> data;
-            return Delete(root -> right , successor -> data);
-            
+        node* current = root;
+        while (true) { 
+            if (val == current->data) {
+                cout << "Element Already Present.\n";
+                delete newnode; 
+                return;
+            } else if (val < current->data) {
+                if (current->left == nullptr) {
+                    current->left = newnode;
+                    cout << "Node Added to Left Side.\n";
+                    return;
+                }
+                current = current->left;
+            } else {
+                if (current->right == nullptr) {
+                    current->right = newnode;
+                    cout << "Node Added to Right Side.\n";
+                    return;
+                }
+                current = current->right;
+            }
         }
     }
-    return root;
-}
 
-void inorder(Node * root){
-    if(root){
+    void display(node* temp, int depth = 0) {
+        if (!temp) return; 
+        display(temp->right, depth + 1);
+        for (int i = 0; i < depth; i++) cout << "     ";
+        cout << temp->data << "\n";
+        display(temp->left, depth + 1);
+    }
+
+    node* Delete(node* root, int key) {
+        if (root == nullptr) return root;
+        if (key < root->data) {
+            root->left = Delete(root->left, key);
+        } else if (key > root->data) {
+            root->right = Delete(root->right, key);
+        } else {
+            if (!root->left && !root->right) {
+                delete root;
+                return nullptr;
+            } else if (!root->left) {
+                node* temp = root->right;
+                delete root;
+                return temp;
+            } else if (!root->right) {
+                node* temp = root->left;
+                delete root;
+                return temp;
+            } else {
+                node* successor = findMin(root->right); 
+                root->data = successor->data;
+                root->right = Delete(root->right, successor->data); 
+            }
+        }
+        return root;
+    }
+
+    int findpath(node* root) {
+        if (!root) return 0;
+        int left = findpath(root->left);
+        int right = findpath(root->right);
+        return max(left, right) + 1;
+    }
+
+    node* findMin(node* root) {
+        while (root && root->left) {
+            root = root->left;
+        }
+        return root; 
+    }
+
+    void inorder(node* root) {
+        if (!root) return;
         inorder(root->left);
-        cout<<root->data<<" ";
+        cout << root->data << " ";
         inorder(root->right);
     }
-}
 
-void preorder(Node *root){
-    if(root){
-        cout<<root -> data <<" ";
+    void preorder(node* root) {
+        if (!root) return;
+        cout << root->data << " ";
         preorder(root->left);
         preorder(root->right);
     }
-}
-    
-void postorder(Node *root){
-    if(root){
-        postorder(root-> left);
-        postorder(root -> right);
-        cout<<root -> data<<" ";
-    }
-}
 
-void BFS(Node * root){
-    if(root == NULL){
-        cout<<"Can't perform BFS.\n";
+    void postorder(node* root) {
+        if (!root) return;
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
     }
-    else{
-        queue<Node *> q;
+
+    node* swaptree(node* temp) {
+        if (!temp) return temp;
+        swap(temp->left, temp->right);
+        swaptree(temp->left);
+        swaptree(temp->right);
+        return temp;
+    }
+
+    void BFS(node* root) {
+        if (!root) {
+            cout << "Tree is empty.\n";
+            return;
+        }
+
+        queue<node*> q;
         q.push(root);
-        while(!q.empty())
-        {
-            Node * temp = q.front();
+
+        while (!q.empty()) {
+            node* temp = q.front();
             q.pop();
-
-            cout<<temp -> data <<" ";
-
-            if(temp -> left)q.push(temp->left);
-            if(temp -> right)q.push(temp -> right);
-        }    
-    }
-}
-
-int search(Node *root, int value){
-    if(root == NULL){
-        return 0;
-
-    }
-    else{
-        if(root -> data == value){
-            return 1;
-        }
-        else if(root -> data > value){
-            return search(root -> left,value);
-        }
-        else{
-            return search(root -> right,value);
+            cout << temp->data << " ";
+            if (temp->left) q.push(temp->left);
+            if (temp->right) q.push(temp->right);
         }
     }
-}
 
-void display(node *ptr, int level){
-    if (ptr != NULL) {
-        display(ptr->right, level + 1);
-        cout << endl;
-        for (int i = 0; i < level; i++)
-            cout << "       ";
-        cout << ptr->data;
-        display(ptr->left, level + 1);
-    }
-}
-
-node *deletenode(node *root, int key){
-    if (!root) return root;
-    if (key < root->data) root->left = deleteNode(root->left, key);
-    else if (key > root->data) root->right = deleteNode(root->right, key);
-    else {
-        if (!root->left) return root->right;
-        else if (!root->right) return root->left;
-        root->data = min(root->right);
-        root->right = deleteNode(root->right, root->info);
-    }
-    return root;
-}
-
-
+   
+    node* getRoot() { return root; }//use this function instead of root because it can update the value
+    void setRoot(node* newRoot) { root = newRoot; }//will not require to assign separately just call this fx.
 };
 
+int main() {
+    int ans, key, choice;
+    BST b;
 
-int main(){
-    Traversal t;
-    Node *root = nullptr;
-    Node *another = nullptr;
-    Node *temp = nullptr;
-    int key;
-    int values[] = {21,27,25,1,28,76,11};
-    for(int value : values){
-        root = t.insertNode(root,value);
-    }
-    int ch,dt,sch,it,find;
-    
-    do{
-        cout<<"Menu :\n";
-        cout<<"1.Insert a new node.\n";
-        cout<<"2.Longest Path.\n";
-        cout<<"3.Minimum Value.\n";
-        cout<<"4.Swap the BST.\n";
-        cout<<"5.Search A Value.\n";
-        cout<<"6.Inorder Traversal.\n";
-        cout<<"7.Preorder Traversal.\n";
-        cout<<"8.Postorder Traversal.\n";
-        cout<<"9.BFS.\n";
-        cout<<"10.Exit.\n";
-        cout<<"11.Display.\n";
-        cout<<"12.Delete a node.\n";
-        cin>>ch;
+    do {
+        cout << "\n-----Menu-----\n";
+        cout << "1. Insert\n";
+        cout << "2. Display\n";
+        cout << "3. Delete\n";
+        cout << "4. Longest Path\n";
+        cout << "5. Minimum Value\n";
+        cout << "6. Inorder\n";
+        cout << "7. Preorder\n";
+        cout << "8. Postorder\n";
+        cout << "9. BFS\n";
+        cout << "10. Swap Tree\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-        switch(ch){
-            case 1 :
-                cout<<"Enter the Data to Insert : ";
-                cin>>dt;
-                t.insertNode(root,dt);
-                cout<<endl;
+        switch (choice) {
+            case 1:
+                cout << "Enter key to insert: ";
+                cin >> key;
+                b.insert(key); 
                 break;
 
-            case 2 :
-                cout<<"Longest Path is : "<<t.longestPath(root);
-                cout<<endl;
+            case 2:
+                cout << "BST Display:\n";
+                b.display(b.getRoot());
                 break;
 
-            case 3 :
-                cout<<"The Minimum value In BST is : "<<t.minValue(root);
-                cout<<endl;
+            case 3:
+                cout << "Enter key to delete: ";
+                cin >> key;
+                b.setRoot(b.Delete(b.getRoot(), key));
                 break;
 
-            case 4 :
-                another = t.swapTree(root);
-                cout<<"Swapped tree is : ";
-                t.inorder(another);
-                cout<<endl;
+            case 4:
+                cout << "Longest Path: " << b.findpath(b.getRoot()) << "\n";
                 break;
 
-            case 5 :
-                {cout<<"Enter the data to Search : ";
-                cin>>sch;
-                find = t.search(root,sch);
-                if(find == 0){
-                    cout<<"Data is absent.\n";
-                }
-                else{
-                    cout<<"Data is present.\n";
-                }
+            case 5: {
+                node* minNode = b.findMin(b.getRoot());
+                if (minNode)
+                    cout << "Minimum Value: " << minNode->data << "\n";
+                else
+                    cout << "Tree is empty.\n";
                 break;
             }
 
-            case 6 :
-                cout<<"Inorder Traversal : ";
-                t.inorder(root);
-                cout<<endl;
+            case 6:
+                cout << "Inorder Traversal:\n";
+                b.inorder(b.getRoot());
+                cout << "\n";
                 break;
 
-            case 7 :
-                cout<<"Preorder Traversal : ";
-                t.preorder(root);
-                cout<<endl;
+            case 7:
+                cout << "Preorder Traversal:\n";
+                b.preorder(b.getRoot());
+                cout << "\n";
                 break;
 
-            case 8 : 
-                cout<<"Postorder Traversal : ";
-                t.postorder(root);
-                cout<<endl;
+            case 8:
+                cout << "Postorder Traversal:\n";
+                b.postorder(b.getRoot());
+                cout << "\n";
                 break;
 
-            case 9 :
-                cout<<"BFS : ";
-                t.BFS(root);
-                cout<<endl;
+            case 9:
+                cout << "BFS Traversal:\n";
+                b.BFS(b.getRoot());
+                cout << "\n";
                 break;
 
-            case 10 :
-                cout<<"Thank You !\n";
+            case 10: {
+                node* newRoot = b.swaptree(b.getRoot());
+                cout << "Swapped Tree Display:\n";
+                b.display(newRoot);
                 break;
+            }
 
-            case 11 :
-                cout<<"BST Traversal.\n";
-                t.display();
-                break;
-
-            case 12 :
-                {
-                    cout<<"Enter a Node to delete :";
-                    cin>>key;
-                cout<<"Deleting a node.\n";
-                temp = t.deletenode(root,key);
-                    break;
-                }
-            
-            default : 
-            cout<<"Invalid Choice, Try Again !!!\n";
-
+            default:
+                cout << "Invalid choice!\n";
         }
 
-        cout<<"Do You Want To Continue ?(1/0)\n";
-        cin>>it;
-    }while(it == 1);
+        cout << "\nPress 1 to continue: ";
+        cin >> ans;
+
+    } while (ans == 1);
 
     return 0;
 }
